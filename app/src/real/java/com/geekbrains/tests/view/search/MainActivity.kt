0339112ef4305d745b660bd276f902ea660be60f
Stas Zareckii <zareckii@gmail.com>
2021-06-28 15:45:40 +0300

    +RXJava

--- app/src/real/java/com/geekbrains/tests/view/search/MainActivity.kt
+++ app/src/real/java/com/geekbrains/tests/view/search/MainActivity.kt
@@ -16,6 +16,7 @@ import com.geekbrains.tests.repository.GitHubRepository
 import com.geekbrains.tests.view.details.DetailsActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
 
@@ -84,6 +85,7 @@ class MainActivity : AppCompatActivity(), ViewSearchContract {
     private fun createRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
@@ -129,6 +131,5 @@ class MainActivity : AppCompatActivity(), ViewSearchContract {
 
     companion object {
         const val BASE_URL = "https://api.github.com"
-//        const val FAKE = "FAKE"
     }
 }
