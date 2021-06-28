5f0e4b148d959ac3d93a776c0f7690cce03f7d20
Stas Zareckii <zareckii@gmail.com>
2021-06-28 17:19:34 +0300

    +ViewModal

--- app/src/main/java/com/geekbrains/tests/presenter/RepositoryContract.kt
+++ app/src/main/java/com/geekbrains/tests/presenter/RepositoryContract.kt
@@ -4,7 +4,7 @@ import com.geekbrains.tests.model.SearchResponse
 import com.geekbrains.tests.repository.RepositoryCallback
 import io.reactivex.Observable
 
-internal interface RepositoryContract {
+interface RepositoryContract {
     fun searchGithub(
         query: String,
         callback: RepositoryCallback
