200a604020ff7893aee8fee9768cc7ed20ed6148
Stas Zareckii <zareckii@gmail.com>
2021-06-29 16:40:29 +0300

    +refactoring

--- app/src/androidTest/java/com/geekbrains/tests/view/search/MainActivityTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/view/search/MainActivityTest.kt
@@ -1,6 +1,5 @@
 package com.geekbrains.tests.view.search
 
-
 import android.view.View
 import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
--- app/src/real/java/com/geekbrains/tests/view/search/SearchViewModel.kt
+++ app/src/real/java/com/geekbrains/tests/view/search/SearchViewModel.kt
@@ -66,8 +66,8 @@ class SearchViewModel(
     }
 }
 
-    sealed class ScreenState {
-        object Loading : ScreenState()
-        data class Working(val searchResponse: SearchResponse) : ScreenState()
-        data class Error(val error: Throwable) : ScreenState()
-    }
+sealed class ScreenState {
+    object Loading : ScreenState()
+    data class Working(val searchResponse: SearchResponse) : ScreenState()
+    data class Error(val error: Throwable) : ScreenState()
+}
